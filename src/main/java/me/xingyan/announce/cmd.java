72416189724b1353640abe94b271a9dc9d7cf7d4
XingYan <46369463+paul2487@users.noreply.github.com>
2021-04-06 19:46:14 +0800

    1.1 update

--- src/main/java/me/xingyan/announce/cmd.java
+++ src/main/java/me/xingyan/announce/cmd.java
@@ -1,8 +1,7 @@
 package me.xingyan.announce;
 
-import github.scarsz.discordsrv.DiscordSRV;
+import github.scarsz.discordsrv.dependencies.jda.api.EmbedBuilder;
 import github.scarsz.discordsrv.dependencies.jda.api.entities.TextChannel;
-import github.scarsz.discordsrv.listeners.DiscordChatListener;
 import github.scarsz.discordsrv.util.DiscordUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -10,6 +9,10 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static me.xingyan.announce.Main.config;
 import static me.xingyan.announce.Main.plugin;
@@ -35,11 +38,33 @@ public class cmd implements CommandExecutor {
             if(discord){
                 String channelid = config.getString("discord.channel");
                 TextChannel channel = DiscordUtil.getTextChannelById(channelid);
-                DiscordUtil.sendMessage(channel, msg);
+                if(config.getBoolean("discord.embed")){
+                    EmbedBuilder embed = new EmbedBuilder();
+                    String title = config.getString("message.title");
+                    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a");
+                    LocalDateTime time = LocalDateTime.now();
+                    embed.setTitle(title);
+                    embed.setDescription(msg);
+                    String sendby = config.getString("message.sendby");
+                    embed.setFooter(sendby+" "+sender.getName()+" | "+dtf.format(time));
+                    embed.setColor(new Color(255,0,0));
+                    channel.sendMessage(embed.build()).queue();
+                }else{
+                    DiscordUtil.sendMessage(channel, msg);
+                }
+
             }
         }
         return true;
     }
+
+    public static Color hex2Rgb(String colorStr) {
+        return new Color(
+                Integer.valueOf( colorStr.substring( 1, 3 ), 16 ),
+                Integer.valueOf( colorStr.substring( 3, 5 ), 16 ),
+                Integer.valueOf( colorStr.substring( 5, 7 ), 16 ) );
+    }
+
 
 }
 
